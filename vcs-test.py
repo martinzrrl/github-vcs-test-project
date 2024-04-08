868c37a3f63b2a59d81fa4800eb3a50ad01d9776
account-for-tests <166156811+account-for-tests@users.noreply.github.com>
2024-04-08 11:41:03 +0200

    Update vcs-test.py
    updated file according to comment

--- vcs-test.py
+++ vcs-test.py
@@ -5,7 +5,6 @@
 import pandas as pd
 
 # add one line of code to  decline below
-import matplotlib.pyplot as plt
 
 
 # this is a test of a commit to the central repository, between the time of the PR and merge of the PR 
